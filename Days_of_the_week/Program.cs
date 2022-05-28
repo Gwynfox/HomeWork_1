051920bbfbf577e794dcdcb7600b1db8a804776d
Gwynfox <105600030+Gwynfox@users.noreply.github.com>
2022-05-29 01:44:22 +0300

    Add project files.

---
+++ Days_of_the_week/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace HomeWork_1.Days_of_the_week    // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Hello! Plaese enter number of the day of the week (1-7).");
+            
+            var nomberWeek = Console.ReadLine();
+            
+            switch (nomberWeek)
+            {
+                case "1":
+                    Console.WriteLine("Monday");
+                    break;
+                case "2":
+                    Console.WriteLine("Tuesday");
+                    break;
+                case "3":
+                    Console.WriteLine("Wednesday");
+                    break;
+                case "4":
+                    Console.WriteLine("Thursday");
+                    break;
+                case "5":
+                    Console.WriteLine("Friday");
+                    break;
+                case "6":
+                    Console.WriteLine("Saturday");
+                    break;
+                case "7":
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("It's not number of the day of the week!!! Please,try again and enter number 1-7");
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HomeWork_1.MultiplicatonTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number for show multiplication table:");
+            var userInput = Console.ReadLine();
+
+            int number = 0;
+
+            var isNumber = int.TryParse(userInput, out number);
+            if (isNumber)
+            {                
+                for (var i = 0; i < 11; i++)
+                {
+                    Console.WriteLine($"{number}*{i}={number * i}");
+                }                                
+            }
+            if (!isNumber)
+            {
+                Console.WriteLine("This is not a number");
+            }
+        }
+    }
+}
